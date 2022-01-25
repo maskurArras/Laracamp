09c986c33e05b55413c30db76ddde0c57cf1be45
maskurArras <dayminoru27@gmail.com>
2022-01-25 18:59:28 +0700

    Integration My Dashboard & Relasi Checkout-Camp

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Checkout;
 use Illuminate\Http\Request;
+use Auth;
 
 class HomeController extends Controller
 {
     public function dashboard()
     {
-        return view('user.dashboard');
+
+
+        $checkouts = Checkout::with('Camp')->whereUserId(Auth::id())->get();
+
+        return view('user.dashboard', [
+            'checkouts' => $checkouts,
+        ]);
     }
 }
--- app/Models/Checkout.php
+++ app/Models/Checkout.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Checkout extends Model
@@ -16,5 +17,16 @@ class Checkout extends Model
     public function setExpiredAttribute($value)
     {
         $this->attributes['expired'] = date('Y-m-t', strtotime($value));
+    }
+
+    // relasi table checkout dengan table camp
+    /**
+     * Get the Camp that owns the Checkout
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function Camp(): BelongsTo
+    {
+        return $this->belongsTo(Camp::class);
     }
 }
