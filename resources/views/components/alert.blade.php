d1ab3076458c64f22b191996f81eff56b5cbea2d
maskurArras <dayminoru27@gmail.com>
2022-01-26 04:06:57 +0700

    alert validation isRegistered

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,4 +1,4 @@
-@if (!message = Session::get('error'))
+@if ($message = Session::get('error'))
 <div class="alert alert-danger alert-dismissible fade show" role="alert">
     <strong>{{ $message }}</strong>
     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
