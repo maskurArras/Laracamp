2ece1d12744c8185c04ded2f4b822b42fb3f9915
maskurArras <dayminoru27@gmail.com>
2022-01-29 07:10:52 +0700

    Switch Admin dashboard and User dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Checkout;
 use Illuminate\Http\Request;
 use Auth;
 
@@ -10,12 +9,15 @@ class HomeController extends Controller
 {
     public function dashboard()
     {
+        switch (Auth::user()->is_admin) {
+            case 'true':
+                return redirect(route('admin.dashboard'));
+                break;
 
+            default:
+                return redirect(route('user.dashboard'));
 
-        $checkouts = Checkout::with('Camp')->whereUserId(Auth::id())->get();
-
-        return view('user.dashboard', [
-            'checkouts' => $checkouts,
-        ]);
+                break;
+        }
     }
 }
