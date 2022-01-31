f727eeae56974f3b61b8db5b99027dea2127cee3
maskurArras <dayminoru27@gmail.com>
2022-02-01 06:09:07 +0700

    Init Midtrans

--- app/Http/Controllers/User/CheckoutController.php
+++ app/Http/Controllers/User/CheckoutController.php
@@ -11,9 +11,25 @@ use Auth;
 // menggunakan package mail
 use Mail;
 use App\Mail\Checkout\AfterCheckout;
+// menggunakan package str
+use Str;
+use Exception;
+// menggunakan package midtrans
+use Midtrans;
 
 class CheckoutController extends Controller
 {
+    // membuat countruct untuk midtarns
+
+    public function __construct()
+    {
+        Midtrans\Config::$serverKey = env('MIDTRANS_SERVERKEY');
+        Midtrans\Config::$isProduction = env('MIDTRANS_IS_PRODUCTION');
+        Midtrans\Config::$isSanitized = env('MIDTRANS_IS_SANITIZED');
+        Midtrans\Config::$is3ds = env('MIDTRANS_IS_3DS');
+    }
+
+
     /**
      * Display a listing of the resource.
      *
@@ -65,6 +81,8 @@ class CheckoutController extends Controller
 
         //create checkout
         $checkout = Checkout::create($data);
+        // midtrans handler
+        $this->getSnapRedirect($checkout);
 
         // sending email
         Mail::to(Auth::user()->email)->send(new AfterCheckout($checkout));
@@ -123,9 +141,60 @@ class CheckoutController extends Controller
         return view('checkout.success');
     }
 
-    // // invoice dengan paramater $caheckout dari Model Checkout
-    // public function invoice(Checkout $checkout)
-    // {
-    //     return $checkout;
-    // }
+    // midtrans Handler
+    public function getSnapRedirect(Checkout $checkout)
+    {
+        $orderId = $checkout->id . '-' . Str::random(5);
+        $price = $checkout->Camp->price * 1000;
+
+        $checkout->midtrans_booking_code = $orderId;
+
+        $transaction_details = [
+            'order_id' => $orderId,
+            'gross_amount' => $price
+        ];
+
+        $item_details[] = [
+            'id' => $orderId,
+            'price' => $price,
+            'quantity' => 1,
+            'name' => "Payment for {$checkout->Camp->title} Camp"
+        ];
+
+        $userData = [
+            'first_name' => $checkout->User->name,
+            'last_name' => '',
+            'address' => $checkout->User->address,
+            'city' => '',
+            'postal_code' => '',
+            'phone' => $checkout->User->phone,
+            'country_code' => 'IDN'
+        ];
+
+        $customer_details = [
+            'first_name' => $checkout->User->name,
+            'last_name' => '',
+            'email' => $checkout->User->email,
+            'phone' => $checkout->User->phone,
+            'billing_address' => $userData,
+            'shipping_address' => $userData
+        ];
+
+        $midtrans_params = [
+            'transaction_details' => $transaction_details,
+            'customer_details' => $customer_details,
+            'item_details' => $item_details,
+        ];
+
+        try {
+            // Get Snap Payment Page Url
+            $paymentUrl = \Midtrans\Snap::createTransaction($midtrans_params)->redirect_url;
+            $checkout->midtrans_url = $paymentUrl;
+            $checkout->save();
+
+            return $paymentUrl;
+        } catch (Exception $e) {
+            return false;
+        }
+    }
 }
